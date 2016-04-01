ccbf03df93b0bb6d40758cd342e594ab828936da
Andrew <meilingandrew@gmail.com>
2016-03-31 21:59:12 -0700

    set up

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+// setting up the vegas slider:
+$(".header").vegas({
+	conver: true,
+    slides: [
+        { src: "/images/slider_1.jpg" },
+        { src: "/images/slider_2.jpg" },
+        { src: "/images/slider_3.jpg" }
+    ]
+});
