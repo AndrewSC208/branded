80d1a95800c6c1b6c7d61d8c597461913db85da6
Andrew <meilingandrew@gmail.com>
2016-04-01 07:59:06 -0700

    added menu button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
 // setting up the vegas slider:
 $(".header").vegas({
 	conver: true,
-    slides: [
-        { src: "/images/slider_1.jpg" },
-        { src: "/images/slider_2.jpg" },
-        { src: "/images/slider_3.jpg" }
-    ]
+   	slides: [
+       	{ src: "/images/slider_1.jpg" },
+       	{ src: "/images/slider_2.jpg" },
+       	{ src: "/images/slider_3.jpg" }
+   	]
 });
+
+
